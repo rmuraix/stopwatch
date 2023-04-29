aa849716d664f10d3f587029a66b2497494402a5
rmuraix <35632215+rmuraix@users.noreply.github.com>
2023-04-29 16:04:35 +0900

    Add lap

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 // This code is based on miyakei1225/React-Hands-on-Stopwatch
 function App() {
   const [time, setTime] = useState<number>(0);
+  const [lapTime, setLapTime] = useState<number[]>([]);
   const [timerId, setTimerId] = useState<NodeJS.Timeout | null>(null);
   const [isRunning, setIsRunning] = useState<boolean>(false);
 
@@ -27,15 +28,38 @@ function App() {
     setTime(0);
     if (timerId) clearInterval(timerId);
     setTimerId(null);
+    setIsRunning(false);
+    setLapTime([]);
   };
+
+  // When the lap button is pressed
+  const handleLap = () => {
+    setLapTime([...lapTime, time]);
+  };
+
+  const lap = lapTime.map((element, index) => {
+    return (
+      <li key={index}>
+        <p>
+          {index + 1}: {(element / 1000).toFixed(2)} s
+        </p>
+      </li>
+    );
+  });
 
   return (
     <>
-      <div className="md:container md:mx-auto">
-        <div className="py-8 text-center text-4xl">
-          {(time / 1000).toFixed(2)} s
-        </div>
-        <div className="flex flex-row text-center">
+      <div className="text-center md:container md:mx-auto">
+        <div className="py-8 text-4xl">{(time / 1000).toFixed(2)} s</div>
+        <div className="flex flex-row">
+          <div className="flex-1">
+            <button
+              onClick={handleReset}
+              className="w-5/6 rounded bg-gray-100 py-2 text-black hover:bg-gray-50"
+            >
+              Reset
+            </button>
+          </div>
           <div className="flex-1">
             <button
               onClick={handleStartStop}
@@ -46,13 +70,14 @@ function App() {
           </div>
           <div className="flex-1">
             <button
-              onClick={handleReset}
+              onClick={handleLap}
               className="w-5/6 rounded bg-gray-100 py-2 text-black hover:bg-gray-50"
             >
-              Reset
+              Lap
             </button>
           </div>
         </div>
+        <ol>{lap}</ol>
       </div>
     </>
   );
